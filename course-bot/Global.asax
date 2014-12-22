dda376a2e40707400109b3adb9d789c7279dade7
Nurul Huda Robin <robin@dexsa.org>
2014-12-22 20:43:56 +0600

    init

---
+++ course-bot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="course_bot.MvcApplication" Language="C#" %>
---
+++ course-bot/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(course_bot.Startup))]
+namespace course_bot
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
